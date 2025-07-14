7edf9e60f8fce4c9a3a26ad7d62d72f154369cf5
tio0o0o0o <taiyoperson@gmail.com>
2025-07-14 12:16:07 +1200

    feat: Create routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./components/Home.jsx";
+import Shop from "./components/Shop.jsx";
+import About from "./components/About.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "shop",
+    element: <Shop />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
